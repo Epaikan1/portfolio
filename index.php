2fc1d756329d16616928a504fdf0b0645cdb1315
PAIKAN <90679347+Epaikan1@users.noreply.github.com>
2022-06-14 13:42:20 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <?php 
-$page_title = "Accueil"; // Nom de la page affichée dans son titre
-$menu_active = "accueil"; // Element du menu qui sera affiché comme actif
-$prefix = "";
+$page_title = "Veille";
+$prefix = "../"; // On rajoute ../ devant le chemin pour importer les fichiers du dossier supérieur
 include($prefix."_header.php");
+
+if (!extension_loaded('pdo')) {
+    echo "<div class='alert alert-danger' role='alert'>L'extension PHP 'PDO' n'est pas activée sur ce serveur.</div>";
+}
+
+// On prépare la connexion à la base de données SQL
+try {
+    $pdo = new PDO("mysql:host=".$DB_HOST.";dbname=".$DB_DATABASE.";charset=utf8", $DB_USERNAME, $DB_PASSWORD);
+} catch (Exception $e) {
+    echo "<div class='alert alert-danger' role='alert'>Erreur de connexion à la base de données.</div>";
+}
 ?>
 
-<h1>Page d'accueil</h1>
+<h1>Veille</h1>
 <p>
-    Ici le contenu HTML de la page d'accueil.
+    Ici le contenu HTML de la page de veille.
+    Voici un <a href="<?=$prefix?>index.php">lien</a> vers la page d'accueil.
 </p>
 
 <?php
